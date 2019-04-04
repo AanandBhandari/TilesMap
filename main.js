6bf4b63ee71578f9a537cb3641f9c1810984c4a0
AanandBhandari <aanandbhandari143@gmail.com>
2019-04-04 11:26:42 +0545

    add cameras movement

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const config = {
     type: Phaser.AUTO, // Which renderer to use
-    width: 171,
-    height: 160,
-    zoom: 3, // Since we're working with 16x16 pixel tiles, let's scale up the canvas by 3x
+    width: 800,
+    height: 600,
     pixelArt: true, // Force the game to scale images up crisply
     parent: "game-container", // ID of the DOM element to add the canvas to
     scene: {
@@ -13,33 +12,45 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let controls;
 
 function preload() {
     // Runs once, loads up assets like images and audio
-    this.load.image("mario-tiles", "/assets/super-mario-tiles.png");
+    this.load.image("tiles", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilesets/catastrophi_tiles_16_blue.png");
+    this.load.tilemapCSV("map", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/catastrophi_level3.csv");
 }
 
 function create() {
-    // Load a map from a 2D array of tile indices
-    const level = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 2, 3, 0, 0, 0, 1, 2, 3, 0],
-        [0, 5, 6, 7, 0, 0, 0, 5, 6, 7, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 14, 13, 14, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 14, 14, 14, 14, 14, 0, 0, 0, 15],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 15, 15],
-        [35, 36, 37, 0, 0, 0, 0, 0, 15, 15, 15],
-        [39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39]
-    ];
-    // When loading from an array, make sure to specify the tileWidth and tileHeight
-    const map = this.make.tilemap({ data: level, tileWidth:16 , tileHeight: 16 });
-    const tiles = map.addTilesetImage("mario-tiles");
-    const layer = map.createStaticLayer(0, tiles, 0, 0);
+    // When loading a CSV map, make sure to specify the tileWidth and tileHeight!
+    const map = this.make.tilemap({ key: "map", tileWidth: 16, tileHeight: 16 });
+    const tileset = map.addTilesetImage("tiles");
+    const layer = map.createStaticLayer(0, tileset, 0, 0); // layer index, tileset, x, y
+    // Phaser supports multiple cameras, but you can access the default camera like this:
+    const camera = this.cameras.main;
+
+    // Set up the arrows to control the camera
+    const cursors = this.input.keyboard.createCursorKeys();
+    controls = new Phaser.Cameras.Controls.FixedKeyControl({
+        camera: camera,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    });
+    // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.add
+        .text(16, 16, "Arrow keys to scroll", {
+            font: "18px monospace",
+            fill: "#ffffff",
+            padding: { x: 20, y: 10 },
+            backgroundColor: "#000000"
+        })
+        .setScrollFactor(0);
 }
 
 function update(time, delta) {
-    // Runs once per frame for the duration of the scene
+    // Apply the controls to the camera each update tick of the game
+    controls.update(delta);
 }
