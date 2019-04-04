fe639dd55da71a867257c236cc075284a630e7cd
AanandBhandari <aanandbhandari143@gmail.com>
2019-04-04 12:54:12 +0545

    tilemap with json

--- main.js
+++ main.js
@@ -15,16 +15,22 @@ const game = new Phaser.Game(config);
 let controls;
 
 function preload() {
-    // Runs once, loads up assets like images and audio
-    this.load.image("tiles", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilesets/catastrophi_tiles_16_blue.png");
-    this.load.tilemapCSV("map", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/catastrophi_level3.csv");
+    this.load.image("tiles", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilesets/tuxmon-sample-32px-extruded.png");
+    this.load.tilemapTiledJSON("map", "https://www.mikewesthad.com/phaser-3-tilemap-blog-posts/post-1/assets/tilemaps/tuxemon-town.json");
 }
+
 
 function create() {
     // When loading a CSV map, make sure to specify the tileWidth and tileHeight!
-    const map = this.make.tilemap({ key: "map", tileWidth: 16, tileHeight: 16 });
-    const tileset = map.addTilesetImage("tiles");
-    const layer = map.createStaticLayer(0, tileset, 0, 0); // layer index, tileset, x, y
+    const map = this.make.tilemap({ key: "map"});
+    // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+    // Phaser's cache (i.e. the name you used in preload)
+    const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+    // Parameters: layer name (or index) from Tiled, tileset, x, y
+    const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+    const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+    const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
     // Phaser supports multiple cameras, but you can access the default camera like this:
     const camera = this.cameras.main;
 
